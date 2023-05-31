e183a01202cec7fe2a83f4c6708cdaa06e086914
daoistcoder <deservekindness@gmail.com>
2023-05-31 16:50:17 +0800

    update form binded with Post route

--- vue-laravel-demo/resources/js/Pages/Users/Create.vue
+++ vue-laravel-demo/resources/js/Pages/Users/Create.vue
@@ -3,54 +3,64 @@
 
     <h1 class="text-3xl">Create New User</h1>
 
-    <form method="POST" action="/" class="max-w-md mx-auto mt-8">
+    <form @submit.prevent="submit" class="max-w-md mx-auto mt-8">
         <div class="mb-6">
-            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+            <label
+                class="block mb-2 uppercase font-bold text-xs text-gray-700"
                 for="name"
             >
                 Name
             </label>
 
-            <input class="border border-gray-400 p-2 w-full"
-                 type="text"
-                 name="name"
-                 id="name"
-                 required
-            >
+            <input
+                v-model="form.name"
+                class="border border-gray-400 p-2 w-full"
+                type="text"
+                name="name"
+                id="name"
+                required
+            />
         </div>
 
         <div class="mb-6">
-            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+            <label
+                class="block mb-2 uppercase font-bold text-xs text-gray-700"
                 for="email"
             >
                 Email
             </label>
 
-            <input class="border border-gray-400 p-2 w-full"
-                 type="email"
-                 name="email"
-                 id="email"
-                 required
-            >
+            <input
+                v-model="form.email"
+                class="border border-gray-400 p-2 w-full"
+                type="email"
+                name="email"
+                id="email"
+                required
+            />
         </div>
 
         <div class="mb-6">
-            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+            <label
+                class="block mb-2 uppercase font-bold text-xs text-gray-700"
                 for="password"
             >
                 Password
             </label>
 
-            <input class="border border-gray-400 p-2 w-full"
-                 type="password"
-                 name="password"
-                 id="password"
-                 required
-            >
+            <input
+                v-model="form.password"
+                class="border border-gray-400 p-2 w-full"
+                type="password"
+                name="password"
+                id="password"
+                required
+            />
         </div>
 
         <div class="mb-6">
-            <button type="submit"
+            <button
+                type="submit"
                 class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
             >
                 Submit
@@ -59,8 +69,17 @@
     </form>
 </template>
 
-<script>
-export default {
+<script setup>
+import { reactive } from "vue";
+import {Inertia} from "@inertiajs/inertia";
 
-}
+let form = reactive({
+    name: "",
+    email: "",
+    password: "",
+});
+
+let submit = () => {
+    Inertia.post('/users', form);
+};
 </script>
