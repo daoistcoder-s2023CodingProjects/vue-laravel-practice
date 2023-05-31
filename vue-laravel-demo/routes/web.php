284084187e8bd87236d35c255fb4c57757541ee1
daoistcoder <deservekindness@gmail.com>
2023-05-31 23:25:48 +0800

    try first vueComp with controller

--- vue-laravel-demo/routes/web.php
+++ vue-laravel-demo/routes/web.php
@@ -1,74 +1,66 @@
 <?php
 
+use \App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use \App\Models\User;
-use Illuminate\Support\Facades\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// login endPoint
+Route::get('/login',[LoginController::class, 'create'])->name('login');
 
-Route::get('/', function () {
-    return Inertia::render('Home');
+// auth endPoint
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Home');
+    });
+
+    Route::get('/nav', function () {
+        return Inertia::render('Nav');
+    });
+
+    Route::get('/users', function () {
+        return Inertia::render('Users/Index', [
+            'users' => User::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate(10)
+                ->withQueryString()
+                ->through(fn ($user) => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ]),
+            'filters' => Request::only(['search'])
+        ]);
+    });
+
+    Route::get('/users/create', function () {
+        return Inertia::render('Users/Create');
+    });
+
+    Route::post('/users', function () {
+        //trottle submit
+        // sleep(3);
+
+        //validate the request
+        $attributes = Request::validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+        //create the user
+        User::create($attributes);
+
+        //redirect
+        return redirect('/users');
+    });
+
+    Route::get('/settings', function () {
+        return Inertia::render('Settings');
+    });
+
+    Route::post('/logout', function () {
+        dd(request('foo'));
+    });
 });
-
-Route::get('/nav', function () {
-    return Inertia::render('Nav');
-});
-
-Route::get('/users', function () {
-    return Inertia::render('Users/Index', [
-        'users' => User::query()
-            ->when(Request::input('search'), function ($query, $search) {
-                $query->where('name', 'like', "%{$search}%");
-            })
-            ->paginate(10)
-            ->withQueryString()
-            ->through(fn($user) => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-            ]),
-        'filters' => Request::only(['search'])
-    ]);
-});
-
-Route::get('/users/create', function () {
-    return Inertia::render('Users/Create');
-});
-
-Route::post('/users', function () {
-    //trottle submit
-    // sleep(3);
-
-    //validate the request
-    $attributes = Request::validate([
-        'name' => 'required',
-        'email' => ['required','email'],
-        'password' => 'required',
-    ]);
-
-    //create the user
-    User::create($attributes);
-
-    //redirect
-    return redirect('/users');
-
-});
-
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
-});
-
-Route::post('/logout', function () {
-    dd(request('foo'));
-});
-
